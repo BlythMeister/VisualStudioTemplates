7d66e4422f35389e38bcd5d74f5e5e13a7368d0c
Chris Blyth <blythmeister@gmail.com>
2020-12-01 10:02:12 +0000

    Add NET5 templates

---
+++ Templates/ProjectTemplates/Visual C#/Paket/Service-NET5/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Hosting;
+
+namespace $safeprojectname$
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            new HostBuilder().Build().Run();
+        }
+    }
+}
